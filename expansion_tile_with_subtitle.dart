982fd159cad891bd4a788de8331ee5845150579f
Mohamed Elrasheid <m7mdra@users.noreply.github.com>
2018-06-26 14:16:04 +0200

    Add files via upload

---
+++ expansion_tile_with_subtitle.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+const Duration _kExpand = const Duration(milliseconds: 200);
+
+class ExpansionSubTitledTile extends StatefulWidget {
+  final Widget leading;
+  final Widget title;
+  final Widget subtitle;
+  final ValueChanged<bool> onExpansionChanged;
+  final List<Widget> children;
+  final Color backgroundColor;
+  final Widget trailing;
+  final bool initiallyExpanded;
+
+  const ExpansionSubTitledTile({
+    Key key,
+    this.leading,
+    @required this.title,
+    this.subtitle,
+    this.backgroundColor,
+    this.onExpansionChanged,
+    this.children = const <Widget>[],
+    this.trailing,
+    this.initiallyExpanded = false,
+  })  : assert(initiallyExpanded != null),
+        super(key: key);
+
+  @override
+  _ExpansionTileState createState() => new _ExpansionTileState();
+}
+
+class _ExpansionTileState extends State<ExpansionSubTitledTile>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  CurvedAnimation _easeOutAnimation;
+  CurvedAnimation _easeInAnimation;
+  ColorTween _borderColor;
+  ColorTween _headerColor;
+  ColorTween _iconColor;
+  ColorTween _backgroundColor;
+  Animation<double> _iconTurns;
+
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(duration: _kExpand, vsync: this);
+    _easeOutAnimation =
+        new CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+    _easeInAnimation =
+        new CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    _borderColor = new ColorTween();
+    _headerColor = new ColorTween();
+    _iconColor = new ColorTween();
+    _iconTurns =
+        new Tween<double>(begin: 0.0, end: 0.5).animate(_easeInAnimation);
+    _backgroundColor = new ColorTween();
+
+    _isExpanded =
+        PageStorage.of(context)?.readState(context) ?? widget.initiallyExpanded;
+    if (_isExpanded) _controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded)
+        _controller.forward();
+      else
+        _controller.reverse().then<void>((Null value) {
+          setState(() {});
+        });
+      PageStorage.of(context)?.writeState(context, _isExpanded);
+    });
+    if (widget.onExpansionChanged != null)
+      widget.onExpansionChanged(_isExpanded);
+  }
+
+  Widget _buildChildren(BuildContext context, Widget child) {
+    final Color borderSideColor =
+        _borderColor.evaluate(_easeOutAnimation) ?? Colors.transparent;
+    final Color titleColor = _headerColor.evaluate(_easeInAnimation);
+
+    return new Container(
+      decoration: new BoxDecoration(
+          color: _backgroundColor.evaluate(_easeOutAnimation) ??
+              Colors.transparent,
+          border: new Border(
+            top: new BorderSide(color: borderSideColor),
+            bottom: new BorderSide(color: borderSideColor),
+          )),
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          IconTheme.merge(
+            data:
+                new IconThemeData(color: _iconColor.evaluate(_easeInAnimation)),
+            child: new ListTile(
+              onTap: _handleTap,
+              leading: widget.leading,
+              title: new DefaultTextStyle(
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subhead
+                    .copyWith(color: titleColor),
+                child: widget.title,
+              ),
+              subtitle: widget.subtitle,
+              trailing: widget.trailing ??
+                  new RotationTransition(
+                    turns: _iconTurns,
+                    child: const Icon(Icons.expand_more),
+                  ),
+            ),
+          ),
+          new ClipRect(
+            child: new Align(
+              heightFactor: _easeInAnimation.value,
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    _borderColor.end = theme.dividerColor;
+    _headerColor
+      ..begin = theme.textTheme.subhead.color
+      ..end = theme.accentColor;
+    _iconColor
+      ..begin = theme.unselectedWidgetColor
+      ..end = theme.accentColor;
+    _backgroundColor.end = widget.backgroundColor;
+
+    final bool closed = !_isExpanded && _controller.isDismissed;
+    return new AnimatedBuilder(
+      animation: _controller.view,
+      builder: _buildChildren,
+      child: closed ? null : new Column(children: widget.children),
+    );
+  }
+}
